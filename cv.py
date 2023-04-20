ea6b46ccf7c37e5568d88ee0e7e85efc72b264f7
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2023-04-20 17:07:33 +0300

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,1 +1,8 @@
 
+import streamlit as st
+
+
+st.set_page_config(page_title="My CV", page_icon=":tada:", layout="wide", color="blue)
+
+#------HEADER SECTION-------
+st.subheader("Hi, I am Thanh Tran :wave:")
