2b03590e2b3094adf4a7f7b457dc6684ff60288f
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2023-04-20 15:46:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
 
-st.write('Hello Tran Viet THien Thanh')
 
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+#------HEADER SECTION-------
+st.subheader("Hi, I am Thanh Tran :wave:")
+st.title("A Third years student of Information Technology at Metropolia University Of Applied Science")
+st.write(" ")
+
+
+#-----WHAT I DO ------
+with st.container()
+    st.write("--- ")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+                """
+                 With broad experiences in Infrastructure, ranging from networking, DevOps and cloud solutions 
+                 I have been working and hands on with multiple school projects such as creating an automated 
+                 pipeline that provisions services to Docker Swarm/Kubernetes using orchestration tools Jenkins
+                 and Github Action.I am familiar with containerization (Docker), orchestration (Ansible, Jenkins)
+                 and cloud solutions (Docker Swarm, Kubernetes)
+                 
+                 """)
+          
