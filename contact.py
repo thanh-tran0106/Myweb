8a863cd5a1111dcc478a4a58dd5d17bec4ae0bc9
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2023-04-20 17:08:29 +0300

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,1 +1,8 @@
 
+import streamlit as st
+
+
+st.set_page_config(page_title="Contact me", page_icon=":tada:", layout="wide")
+
+#------HEADER SECTION-------
+st.subheader("Hi, I am Thanh Tran :wave:")
