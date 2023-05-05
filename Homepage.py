fae30d16c0dee2c3cffc537a5c2a2948bae4a208
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2023-05-05 12:19:02 +0300

    Update Homepage.py
    aaa

--- Homepage.py
+++ Homepage.py
@@ -11,8 +11,33 @@ resume_file = current_dir / "images" / "CV.pdf"
 profile_pic = current_dir / "images" / "profile-pic.png"
 
 
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | Thanh Tran"
+PAGE_ICON = ":wave:"
+NAME = "Thanh Tran"
+DESCRIPTION = """
+Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+"""
+EMAIL = "tranviet.thienthanh@gmail.com"
+SOCIAL_MEDIA = {
+    "YouTube": "https://youtube.com/c/codingisfun",
+    "LinkedIn": "https://linkedin.com",
+    "GitHub": "https://github.com",
+    "Twitter": "https://twitter.com",
+}
+PROJECTS = {
+    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
+    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
+    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
+    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+}
 
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+
+
 
 st.sidebar.success("Select a demo above.")
 
